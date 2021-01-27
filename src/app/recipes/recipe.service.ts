683148c4e8bc534fb382f4aaf90b9db5906aa525
kholidahmad <ahmatcholid@gmail.com>
2021-01-27 20:17:58 +0700

    first fix setup

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,16 +10,23 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      'Spaghetti Jowo',
+      'Perpaduan sepektakular keju dan Mayonaise',
+      'assets/img/pizza.png',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe('Big Fat Burger',
       'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      'assets/img/burger.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
+    new Recipe('Al Kebbabs',
+      'Arabian Jos',
+      'assets/img/kebab.png',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
